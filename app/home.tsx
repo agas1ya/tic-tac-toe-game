0c81b5fdc0c62d95c5e6be03759102ba58ed0ba1
Agastya Dubey <agastyas11@gmail.com>
2025-11-10 18:24:53 +1100

    revised folder stucture

--- app/home.tsx
+++ app/home.tsx
@@ -3,8 +3,8 @@ import Game from "./board";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Tic Tac Toe Game" },
+    { name: "description", content: "A simple tic-tac-toe game built with React" },
   ];
 }
 
